5721a9ce67ceb9e168135d7eda39a8e39bde3f65
jinen abdelhak <jinenab123@gmail.com>
2019-10-19 14:48:32 +0100

    get familiar with yargs

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const validator = require('validator')
+const yargs = require('yargs')
 const chalk = require('chalk')
 const getNotes = require('./notes')
 getNotes('hi')
 console.log(validator.isEmail("jinen"));
 console.log(validator.isURL("ww.jinen.com"));
 console.log(chalk.green.underline.bold("Success!"))
+console.log(chalk.blue.inverse(process.argv[1]))
+console.log(yargs.argv);
