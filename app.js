61f3d3f4165c5024202288d900cd1d5829d83a44
jinen abdelhak <jinenab123@gmail.com>
2019-10-19 14:16:03 +0100

    get familiar with chalk

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes')
 getNotes('hi')
+console.log(validator.isEmail("jinen"));
+console.log(validator.isURL("ww.jinen.com"));
+console.log(chalk.green.underline.bold("Success!"))
